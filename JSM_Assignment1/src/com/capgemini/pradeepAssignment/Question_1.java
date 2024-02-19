7fc067ca922c37fc42f3df567413df280f3b7c78
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-02-19 16:08:06 +0530

    Rename File to Question 1

--- JSM_Assignment1/src/com/capgemini/pradeepAssignment/Question_1.java
+++ JSM_Assignment1/src/com/capgemini/pradeepAssignment/Question_1.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MainClass {
+public class Question_1 {
 	
 	public static void main(String[] args) throws ClassNotFoundException,SQLException {
 		
